12bcf7cd20285641db33dbe5e455f5da1900b9a9
Vidya1623 <168904821+Vidya1623@users.noreply.github.com>
2024-05-14 03:51:01 -0700

    Add files via upload

---
+++ AnagramChecker.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class AnagramChecker {
+
+    public static boolean isAnagram(String str1, String str2) {
+        
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "silent";
+        String str2 = "listen";
+
+        boolean isAnagram = isAnagram(str1, str2);
+
+        if (isAnagram) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
