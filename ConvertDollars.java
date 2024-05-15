dce177048fe6806070a875b87420e615829370aa
Vidya1623 <168904821+Vidya1623@users.noreply.github.com>
2024-05-15 16:40:00 +0530

    Add files via upload

---
+++ ConvertDollars.java
@@ -0,0 +1,7 @@
+public class ConvertDollars {
+    public static void main(String[] args) {
+        int amount = 2000;
+        amount += 1; 
+        System.out.println("The converted amount is: $" + amount); 
+    }
+}
